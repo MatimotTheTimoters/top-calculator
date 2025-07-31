53d93d37e13f99dd3585ada05c013445ec708363
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-07-31 14:50:04 +0800

    Add event listeners
    Work on task 7: Add event listeners to query selectors
        - [x] Special buttons
            - [x] Fix Backspace slicing from the start
                - Use slice(0, -1) to slice from end
                - Assign value to input.textContent
            - [ ] Fix Negate not working two-way
            - [ ] Fix Calculate resulting in NaN when adding two numbers
            - [x] Fix Negate and Calculate working while input is empty
        - [x] Operator buttons
        - [x] Number buttons

--- README.md
+++ README.md
@@ -38,3 +38,14 @@
         - [x] Fix document selector not valid when using "1" ... as property names
             - Rename ID to their alphabetical counterparts (1 -> one)
             - Do the same to property names instead of using strings ("1" -> one)
+
+7. Add event listeners to query selectors
+    - [x] Special buttons
+        - [x] Fix Backspace slicing from the start
+            - Use slice(0, -1) to slice from end
+            - Assign value to input.textContent
+        - [ ] Fix Negate not working two-way
+        - [ ] Fix Calculate resulting in NaN when adding two numbers
+        - [x] Fix Negate and Calculate working while input is empty
+    - [x] Operator buttons
+    - [x] Number buttons
--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let getLastChar = (str) => {
+    let length = str.length;
+    return str.at(length - 1);
+}
+let inputIsFull = (str) => {
+    if (str.length <= 18) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+let thisNum, nextNum;
 let input = document.querySelector(".user-input");
 let output = document.querySelector(".operation-output");
 
@@ -9,12 +22,65 @@ let specBtns = {
     calculate: document.querySelector("#calculate"),
 };
 
+specBtns.allClear.addEventListener("click", () => {
+    input.textContent = "";
+    output.textContent = "";
+});
+specBtns.backSpace.addEventListener("click", () => {
+    input.textContent = input.textContent.slice(0, -1);
+});
+specBtns.negate.addEventListener("click", () => {
+    let firstItem = input.textContent.at(0);
+    if (firstItem !== "-") {
+        input.textContent = `${"-"}${input.textContent}`;
+    } else if (firstItem === "-") {
+        input.textContent.slice(1);
+    }
+});
+specBtns.decimal.addEventListener("click", () => {
+    let lastItem = input.textContent.at(input.textContent.length - 1);
+    if (lastItem !== ".") {
+        input.textContent += ".";
+    } else if (lastItem === ".") {
+        return;
+    }
+});
+specBtns.calculate.addEventListener("click", () => {
+    if (input.textContent.length === 0) {
+        return;
+    }
+
+    output.textContent = Number(input.textContent);
+    input.textContent = "";  
+});
+
 let opBtns = {
     divide: document.querySelector("#divide"),
     multiply: document.querySelector("#multiply"),
     subtract: document.querySelector("#subtract"),
     add: document.querySelector("#add"),
 }
+
+opBtns.divide.addEventListener("click", () => {
+    if (getLastChar(input.textContent) !== "/") {
+        input.textContent += "/";
+    }
+});
+opBtns.multiply.addEventListener("click", () => {
+    if (getLastChar(input.textContent) !== "*") {
+        input.textContent += "*";
+    }
+});
+opBtns.subtract.addEventListener("click", () => {
+    if (getLastChar(input.textContent) !== "-") {
+        input.textContent += "-";
+    }
+});
+opBtns.add.addEventListener("click", () => {
+    if (getLastChar(input.textContent) !== "+") {
+        input.textContent += "+";
+    }
+})
 
 let numBtns = {
     zero: document.querySelector("#zero"),
@@ -28,3 +94,60 @@ let numBtns = {
     eight: document.querySelector("#eight"),
     nine: document.querySelector("#nine"),
 }
+
+numBtns.zero.addEventListener("click", () => {
+    if (!(inputIsFull(input.textContent))) {
+        if(input.textContent.length === 0) {
+            output.textContent += "0";
+            thisNum = 0;
+        } else {
+            input.textContent += "0";
+            nextNum = 0;
+        }
+    }
+});
+numBtns.one.addEventListener("click", () => {
+    if (!(inputIsFull(input.textContent))) {
+        input.textContent += "1";
+    }
+});
+numBtns.two.addEventListener("click", () => {
+    if (!(inputIsFull(input.textContent))) {
+        input.textContent += "2";
+    }
+});
+numBtns.three.addEventListener("click", () => {
+    if (!(inputIsFull(input.textContent))) {
+        input.textContent += "3";
+    }
+});
+numBtns.four.addEventListener("click", () => {
+    if (!(inputIsFull(input.textContent))) {
+        input.textContent += "4";
+    }
+});
+numBtns.five.addEventListener("click", () => {
+    if (!(inputIsFull(input.textContent))) {
+        input.textContent += "5";
+    }
+});
+numBtns.six.addEventListener("click", () => {
+    if (!(inputIsFull(input.textContent))) {
+        input.textContent += "6";
+    }
+});
+numBtns.seven.addEventListener("click", () => {
+    if (!(inputIsFull(input.textContent))) {
+        input.textContent += "7";
+    }
+});
+numBtns.eight.addEventListener("click", () => {
+    if (!(inputIsFull(input.textContent))) {
+        input.textContent += "8";
+    }
+});
+numBtns.nine.addEventListener("click", () => {
+    if (!(inputIsFull(input.textContent))) {    
+        input.textContent += "9";
+    }
+});
